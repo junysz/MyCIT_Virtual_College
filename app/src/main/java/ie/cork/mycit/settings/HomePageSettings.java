8ab448585cc6283968b048886c54810b7241becc
Edmond-Donegan <e.donegan@mycit.ie>
2015-03-13 15:03:06 +0000

    Home Page Settings Changes

--- app/src/main/java/ie/cork/mycit/settings/HomePageSettings.java
+++ app/src/main/java/ie/cork/mycit/settings/HomePageSettings.java
@@ -1,5 +1,127 @@
 package ie.cork.mycit.settings;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.view.View;
+import ie.cork.mycit.group1.R;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import ie.cork.mycit.group1.HomePage;
+import ie.cork.mycit.group1.R;
 
+
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_settings);
+        setTitle("Settings");
+        start();
+    }
+
+    public void start(){
+        startBoxes("Student E-mail", 0, findViewById(R.id.checkBox1));
+        startBoxes("Blackboard", 1, findViewById(R.id.checkBox2));
+        startBoxes("Timetables", 2, findViewById(R.id.checkBox3));
+        startBoxes("Exam Papers", 3, findViewById(R.id.checkBox4));
+        startBoxes("Library Search", 4, findViewById(R.id.checkBox5));
+        startBoxes("Student Card Top-Up", 5, findViewById(R.id.checkBox6));
+        startBoxes("Web for Student", 6, findViewById(R.id.checkBox7));
+        startBoxes("CIT Password Reset", 7, findViewById(R.id.checkBox8));
+        startBoxes("Wifi Registration", 8, findViewById(R.id.checkBox9));
+        startBoxes("Student Drive", 9, findViewById(R.id.checkBox10));
+        startBoxes("Job Listings", 10, findViewById(R.id.checkBox11));
+    }
+
+    public void startBoxes(String text, int x, View v){
+        CheckBox cb = (CheckBox) v;
+        cb.setText(text);
+        cb.setChecked(getFromSP("x"));
+        cb.setOnCheckedChangeListener(this);
+    }
+
+    private boolean getFromSP(String key){
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("PROJECT_NAME", android.content.Context.MODE_PRIVATE);
+        return preferences.getBoolean(key, false);
+    }
+
+    private void saveInSp(String key,boolean value){
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("PROJECT_NAME", android.content.Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    public void save(View v) {
+        saveBoxes(0, findViewById(R.id.checkBox1));
+        saveBoxes(1, findViewById(R.id.checkBox2));
+        saveBoxes(2, findViewById(R.id.checkBox3));
+        saveBoxes(3, findViewById(R.id.checkBox4));
+        saveBoxes(4, findViewById(R.id.checkBox5));
+        saveBoxes(5, findViewById(R.id.checkBox6));
+        saveBoxes(6, findViewById(R.id.checkBox7));
+        saveBoxes(7, findViewById(R.id.checkBox8));
+        saveBoxes(8, findViewById(R.id.checkBox9));
+        saveBoxes(9, findViewById(R.id.checkBox10));
+        saveBoxes(10, findViewById(R.id.checkBox11));
+        try{
+            saveFile();
+        } catch (Exception e) {
+            // e.printStackTrace();
+        }
+        Intent homepage = new Intent(HomePageSettings.this, HomePage.class);
+        startActivity(homepage);
+    }
+
+    public void saveBoxes(int x, View v) {
+        CheckBox cb = (CheckBox) v;
+        if(cb.isChecked() == true){
+            checkList.add(x, "true");
+        }
+        else {
+            checkList.add(x, "false");
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView,
+                                 boolean isChecked) {
+        // TODO Auto-generated method stub
+        switch(buttonView.getId()){
+            case R.id.checkBox1:
+                saveInSp("cb1",isChecked);
+                break;
+            case R.id.checkBox2:
+                saveInSp("cb2",isChecked);
+                break;
+
+            case R.id.checkBox3:
+                saveInSp("cb3",isChecked);
+                break;
+
+            case R.id.checkBox4:
+                saveInSp("cb4",isChecked);
+                break;
+        }
+
+    }
+}
+/*
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
@@ -7,14 +129,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import ie.cork.mycit.group1.HomePage;
 import ie.cork.mycit.group1.R;
 
@@ -116,4 +235,4 @@ public class HomePageSettings extends ActionBarActivity {
 
     }
 
-}
+}*/
