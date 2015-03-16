776db07f26546da68d6bd514646002a221d724f7
Daniel Junyszek <daniel.junyszek@mycit.ie>
2015-03-16 15:08:20 +0000

    timetable classes imported

---
+++ app/src/timetable/ClassComparator.java
@@ -0,0 +1,44 @@
+package ie.cork.mycit.group1.timetable;
+
+import java.util.Comparator;
+/**
+ * 
+ * @author Daniel Junyszek <daniel.junyszek@mycit.ie>
+ * 
+ * 
+ * Software Development Group project Year 3   -   2015
+ * 
+ */
+public class ClassComparator implements Comparator<Lclass> {
+	public int compare(Lclass a , Lclass b)
+	{
+		int result;
+		boolean aDayIsBiggerThanB = a.getDayOfTheWeek()>b.getDayOfTheWeek();
+		boolean aTimeIsBiggerThanB = a.getStartTime().getHours()>b.getStartTime().getHours();
+		if(aDayIsBiggerThanB)
+		{
+			result =  1;
+		}
+		else if(!aDayIsBiggerThanB)
+		{
+			result = -1;
+		}
+		else
+		{
+			if(aTimeIsBiggerThanB)
+			{
+				result = 1;
+			}
+			else if(!aTimeIsBiggerThanB)
+			{
+				result = -1;
+			}
+			else
+			{
+				result = 0;
+			}
+		}
+		//System.out.println("This times : " + a.getStartTime().getHours()+ " , " + b.getStartTime().getHours()+ " and these days : "+ a.getDayOfTheWeek() + " , " +b.getDayOfTheWeek() + " gave result" + result);
+		return result;
+	}
+}
---
+++ app/src/timetable/Time.java
@@ -0,0 +1,28 @@
+package ie.cork.mycit.group1.timetable;
+/**
+ * 
+ * @author Daniel Junyszek <daniel.junyszek@mycit.ie>
+ * 
+ * 
+ * Software Development Group project Year 3   -   2015
+ * 
+ */
+public class Time {
+	private int hours,minutes;
+	public Time(int hours , int minutes)
+	{
+		this.hours=hours;
+		this.minutes=minutes;
+	}
+	public int getHours() {
+		return hours;
+	}
+	public int getMinutes() {
+		return minutes;
+	}
+	public String getTimeFormatted()
+	{
+		
+		return ""+hours+":"+minutes;
+	}
+}
