07ce1b55056c11e0123a638b52d75fa1a3ac9b20
Merotfl <Adrian@My-own-damn-internet.local>
2015-03-23 14:05:48 +0000

    Update
    - Important Documents Done
    - Student Handbooks Done
    - updated Strings.xml

--- app/src/main/java/ie/cork/mycit/group1/StudentHandbooks.java
+++ app/src/main/java/ie/cork/mycit/group1/StudentHandbooks.java
@@ -1,23 +1,74 @@
 package ie.cork.mycit.group1;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class StudentHandbooks extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_student_handbooks);
-	}
+public class StudentHandbooks extends ActionBarActivity {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		return true;
-	}
+    List<String> studentHandbooksNames = new ArrayList<String>();
+    List<String> studentHandbooksLinks = new ArrayList<String>();
+    String tempName = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_handbooks);
+
+        Resources res = this.getResources();
+        String[] names = res.getStringArray(R.array.studentHandbooksNames);
+        String[] links = res.getStringArray(R.array.studentHandbooksLinks);
+        studentHandbooksNames = Arrays.asList(names);
+        studentHandbooksLinks = Arrays.asList(links);
+
+        ListView listnames = (ListView) findViewById(R.id.listViewStudentHandbooks);
+
+        CustomViewAdapter adapter = new CustomViewAdapter(StudentHandbooks.this, Arrays.asList(names));
+        listnames.setAdapter(adapter);
+
+        registerClickCallback();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        return true;
+    }
+
+    private void registerClickCallback() {
+        ListView list = (ListView) findViewById(R.id.listViewStudentHandbooks);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                tempName = studentHandbooksNames.get(position);
+                int x = position + 1;
+                String message = tempName + " opening";
+                Toast.makeText(StudentHandbooks.this, message, Toast.LENGTH_LONG).show();
+                viewWeb(studentHandbooksNames.get(position).toString(), studentHandbooksLinks.get(position).toString());
+            }
+
+        });
+    }
+
+    public void viewWeb(String title, String url) {
+        Intent web = new Intent(StudentHandbooks.this, Web.class);
+        web.putExtra("title", title);
+        String googleDocsLink = "https://docs.google.com/gview?url=";
+        googleDocsLink += url;
+        web.putExtra("url", googleDocsLink);
+        startActivity(web);
+    }
 
 }
