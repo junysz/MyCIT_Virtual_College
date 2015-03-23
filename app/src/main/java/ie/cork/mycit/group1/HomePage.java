ed1b44cedd82127e7871accb2f6e3af766f53fe6
Edmond-Donegan <e.donegan@mycit.ie>
2015-03-23 14:58:43 +0000

    start of the database work
    just setting them up nothing functional yet

--- app/src/main/java/ie/cork/mycit/group1/HomePage.java
+++ app/src/main/java/ie/cork/mycit/group1/HomePage.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -23,6 +25,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+
+import ie.cork.mycit.database.DatabaseSetUp;
 import ie.cork.mycit.settings.HomePageSettings;
 import ie.cork.mycit.timetable.Timetables;
 
@@ -38,10 +42,16 @@ public class HomePage extends ActionBarActivity implements
     List<String> appLinks = new ArrayList<String>();
     String tempApp = "";
 
+    SQLiteOpenHelper dbHelper;
+    SQLiteDatabase database;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_page);
+
+        dbHelper = new DatabaseSetUp(this);
+        database = dbHelper.getWritableDatabase();
 
         setupTwitterButton();
         setupFacebookButton();
@@ -354,4 +364,5 @@ public class HomePage extends ActionBarActivity implements
             }
         });
     }
+
 }
